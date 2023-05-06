d2a1aeb6ee0c9949ed0a126283347044d2b91355
Dawid Segieta <buler43221@gmail.com>
2023-05-06 15:30:21 +0200

    routeError

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import Post from './components/pages/Post/Post';
+import PostAdd from './components/pages/PostAdd/PostAdd';
+import PostEdit from './components/pages/PostEdit/PostEdit';
+import About from './components/pages/About/About';
+import NotFound from './components/pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
+      <p>Hello world</p>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/post/:id' element={<Post />} />
+        <Route path='/post/add' element={<PostAdd />} />
+        <Route path='/post/edit/:id' element={<PostEdit />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
