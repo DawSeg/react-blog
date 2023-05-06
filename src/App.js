76e5ff6d42c0580f4a1cddea209b910deec8b7bb
Dawid Segieta <buler43221@gmail.com>
2023-05-06 17:17:43 +0200

    HomePageError

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/pages/Home/Home';
-import Post from './components/pages/Post/Post';
-import PostAdd from './components/pages/PostAdd/PostAdd';
-import PostEdit from './components/pages/PostEdit/PostEdit';
-import About from './components/pages/About/About';
-import NotFound from './components/pages/NotFound/NotFound';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home/Home";
+import Post from "./components/pages/Post/Post";
+import PostAdd from "./components/pages/PostAdd/PostAdd";
+import PostEdit from "./components/pages/PostEdit/PostEdit";
+import About from "./components/pages/About/About";
+import NotFound from "./components/pages/NotFound/NotFound";
+import { Container } from "react-bootstrap";
+import Header from "./components/views/Header/Header";
+import Footer from "./components/views/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello world</p>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/post/:id' element={<Post />} />
-        <Route path='/post/add' element={<PostAdd />} />
-        <Route path='/post/edit/:id' element={<PostEdit />} />
-        <Route path='/about' element={<About />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </div>
+    <main>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="/post/add" element={<PostAdd />} />
+          <Route path="/post/edit" element={<PostEdit />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Container>
+    </main>
   );
 }
 
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,9 +1,26 @@
-const Home = () => {
-    return (
-      <div>
-        <h1>Home</h1>
-      </div>
-    );
-  };
+import { Button, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import AllPosts from "../../features/AllPosts/AllPosts";
 
-  export default Home;
+const Home = () => {
+  return (
+    <>
+      <Row>
+        <Col className="d-flex justify-content-between">
+          <h1>All posts</h1>
+          <Button
+            className="mb-3"
+            variant="outline-primary"
+            as={Link}
+            to={"/post/add"}
+          >
+            Add post
+          </Button>
+        </Col>
+      </Row>
+      <AllPosts />
+    </>
+  );
+};
+
+export default Home;
