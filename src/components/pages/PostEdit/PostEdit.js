61936e82317c523b8ecb8c186650c093c6eb0a9d
Dawid Segieta <buler43221@gmail.com>
2023-05-10 21:09:01 +0200

    addOptionEditPost

--- src/components/pages/PostEdit/PostEdit.js
+++ src/components/pages/PostEdit/PostEdit.js
@@ -1,7 +1,10 @@
+import EditPostForm from '../../features/EditPostForm/EditPostForm';
+
 const PostEdit = () => {
     return (
       <div>
         <h1>PostEdit</h1>
+        <EditPostForm />
       </div>
     );
   };
